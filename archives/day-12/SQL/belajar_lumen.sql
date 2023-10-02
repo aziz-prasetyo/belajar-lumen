910e2e565bfb9578332bc76fa8f542a82c2649eb
Muhammad Aziz Prasetyo <azizprasetyo100901@gmail.com>
2023-10-03 00:03:46 +0700

    Belajar routing untuk API dan kirim email menggunakan Mailtrap

---
+++ archives/day-12/SQL/belajar_lumen.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 02, 2023 at 05:01 PM
+-- Server version: 8.0.30
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `belajar_lumen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int UNSIGNED NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `api_token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `api_token`) VALUES
+(1, 'aziz_prasetyo', 'belajar_lumen12345678');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 /*
@@ -15,4 +19,151 @@
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->group(['prefix' => 'penjualan'], function () use ($router) {
+    $router->get('/', function () {
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil mendapatkan semua data penjualan.',
+            'data' => array(
+                array(
+                    'id' => '7XtY2wZa',
+                    'nomor' => 'SALE-3fQx',
+                    'customer' => 'Eka Jaya'
+                ),
+                array(
+                    'id' => 'Z6mP9rLs',
+                    'nomor' => 'SALE-v8nT',
+                    'customer' => 'Gita Darma'
+                ),
+                array(
+                    'id' => 'q3Wn5yRz',
+                    'nomor' => 'SALE-1XcK',
+                    'customer' => 'Indra Adi'
+                ),
+                array(
+                    'id' => '4sF8mNpQ',
+                    'nomor' => 'SALE-j7Lw',
+                    'customer' => 'Joko Irawan'
+                ),
+                array(
+                    'id' => 'R2y7LwX9',
+                    'nomor' => 'SALE-W6pZ',
+                    'customer' => 'Ahmad Gunawan'
+                ),
+                array(
+                    'id' => 'n8T3fQxK',
+                    'nomor' => 'SALE-mNpQ',
+                    'customer' => 'Budi Hari'
+                ),
+                array(
+                    'id' => 'j7Lw1XcZ',
+                    'nomor' => 'SALE-yRzT',
+                    'customer' => 'Citra Fitri'
+                ),
+                array(
+                    'id' => 'W6pZv8nS',
+                    'nomor' => 'SALE-2wZa',
+                    'customer' => 'Dewi Cahya'
+                ),
+                array(
+                    'id' => 'mNpQ3fQx',
+                    'nomor' => 'SALE-9rLs',
+                    'customer' => 'Eka Erlangga'
+                ),
+                array(
+                    'id' => 'yRzT7XtY',
+                    'nomor' => 'SALE-5yRz',
+                    'customer' => 'Fajar Bintang'
+                )
+            )
+        ]);
+    });
+
+    $router->get('/{id}', function ($id) {
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil mendapatkan data penjualan berdasarkan id.',
+            'data' => array(
+                'id' => $id,
+                'nomor' => 'SALE-9rLs',
+                'customer' => 'Muhammad Aziz Prasetyo'
+            )
+        ]);
+    });
+
+    $router->post('/', function (Request $request) {
+        $nomor = $request->input('nomor');
+        $customer = $request->input('customer');
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil membuat data penjualan baru.',
+            'data' => array(
+                'id' => '5Hku9xTu',
+                'nomor' => $nomor,
+                'customer' => $customer
+            )
+        ]);
+    });
+
+    $router->put('/{id}', function (Request $request, $id) {
+        $nomor = $request->get('nomor');
+        $customer = $request->get('customer');
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil memperbarui data penjualan berdasarkan id.',
+            'data' => array(
+                'id' => $id,
+                'nomor' => $nomor,
+                'customer' => $customer
+            )
+        ]);
+    });
+
+    $router->delete('/{id}', function ($id) {
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil menghapus data penjualan berdasarkan id.'
+        ]);
+    });
+
+    $router->get('/{id}/confirm', function (Request $request, $id) {
+        $user = $request->user();
+
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'error' => 'Unauthorized'
+            ], 401);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil mengkonfirmasi penjualan berdasarkan id dan api_key.'
+        ]);
+    });
+
+    $router->get('/{id}/send-email', function (Request $request, $id) {
+        $user = $request->user();
+
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'error' => 'Unauthorized'
+            ], 401);
+        }
+
+        Mail::raw('Ini adalah email body.', function ($message) {
+            $message->to('azizcogan59@gmail.com')
+                ->subject('Belajar Lumen by Aziz');
+        });
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil mengirimkan email kepada user berdasarkan id.'
+        ]);
+    });
 });
